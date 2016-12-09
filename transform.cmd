bad9e64567010d4989ddcf4c56c8ec4aa0e4886d
Ralf Handl <ralf.handl@sap.com>
2016-12-09 15:05:11 +0100

    transform /scn for producing HTML files for SCN

--- transform.cmd
+++ transform.cmd
@@ -8,13 +8,19 @@ set PROXY=-x proxy:8080
 set CLASSPATH=%CLASSPATH%;C:\eclipse-Luna\plugins\org.apache.xml.serializer_2.7.1.v201005080400.jar;C:\eclipse-Luna\plugins\org.apache.xalan_2.7.1.v201005080400.jar
 set done=false
 
-for /F "eol=# tokens=1,2,3,4" %%F in (%~n0.txt) do (
+set SCN=%2
+if /I [%1] == [/scn] (
+  set SCN=/scn
+  shift /1
+)
+
+for /F "eol=# tokens=1" %%F in (%~n0.txt) do (
 	if /I [%1] == [%%~nF] (
 	    set done=true
-		call :process %%F %%G %%H %%I
+		call :process %%F %SCN%
 	) else if [%1]==[] (
 	    set done=true
-		call :process %%F %%G %%H %%I
+		call :process %%F %SCN%
 	)
 )
 
@@ -26,25 +32,29 @@ exit /b
 
 :process
 
-  echo %~n1
+  <nul (set/p _any=%~n1)
 
   java.exe org.apache.xalan.xslt.Process -XSL C:\git\odata-vocabularies\tools\Vocab-to-MarkDown.xsl -PARAM use-alias-as-filename YES -PARAM odata-vocabularies-url https://github.com/oasis-tcs/odata-vocabularies/blob/master/vocabularies/ -IN %1 -OUT %~n1.md
   git.exe --no-pager diff %~n1.md
 
-  rem curl.exe -k -s %PROXY% --data-binary @%~n1.md -H "Content-Type: text/plain" https://api.github.com/markdown/raw -o %~n1.html
-  curl.exe -k -s --data-binary @%~n1.md -H "Content-Type: text/plain" https://github.wdf.sap.corp/api/v3/markdown/raw -o %~n1.html
+  if /I [%2] == [/scn] (
+    <nul (set/p _any=...)
+    rem curl.exe -k -s %PROXY% --data-binary @%~n1.md -H "Content-Type: text/plain" https://api.github.com/markdown/raw -o %~n1.html
+    curl.exe -k -s --data-binary @%~n1.md -H "Content-Type: text/plain" https://github.wdf.sap.corp/api/v3/markdown/raw -o %~n1.html
 
-  sed.exe -e "s/<a name=\"user-content-/^<a name=\"/g" ^
-          -e "s/<span aria-hidden=\"true\" class=\"octicon octicon-link\"><\/span>//g" ^
-          -e "s/<a href=\"Org\.OData\./^<a href=\"https:\/\/github.com\/oasis-tcs\/odata-vocabularies\/blob\/master\/vocabularies\/Org\.OData\./g" ^
-          -e "s/<a id=\"user-content-[[:alpha:]-]\+\" class=\"anchor\" href=\"#[[:alpha:]-]\+\" aria-hidden=\"true\"><\/a>//g" ^
-          -e "s/\"Common\.xml\"/\"https:\/\/wiki.scn.sap.com\/wiki\/download\/attachments\/448470974\/Common.xml?api=v2\"/g" ^
-          -e "s/\"Communication\.xml\"/\"https:\/\/wiki.scn.sap.com\/wiki\/download\/attachments\/448470971\/Communication.xml?api=v2\"/g" ^
-          -e "s/\"UI\.xml\"/\"https:\/\/wiki.scn.sap.com\/wiki\/download\/attachments\/448470968\/UI.xml?api=v2\"/g" ^
-          -e "s/com\.sap\.vocabularies\.\([^.]\+\)\.v1\.md#/https:\/\/wiki.scn.sap.com\/wiki\/display\/EmTech\/OData+4.0+Vocabularies+-+SAP+\1#/g" ^
-          -e "s/\"Common\.md#/\"https:\/\/wiki.scn.sap.com\/wiki\/display\/EmTech\/OData+4.0+Vocabularies+-+SAP+Common#/g" ^
-          -e "s/\"Communication\.md#/\"https:\/\/wiki.scn.sap.com\/wiki\/display\/EmTech\/OData+4.0+Vocabularies+-+SAP+Communication#/g" ^
-          %~n1.html > %~n1.scn 
-  del %~n1.html
+    sed.exe -e "s/<a name=\"user-content-/^<a name=\"/g" ^
+            -e "s/<span aria-hidden=\"true\" class=\"octicon octicon-link\"><\/span>//g" ^
+            -e "s/<a href=\"Org\.OData\./^<a href=\"https:\/\/github.com\/oasis-tcs\/odata-vocabularies\/blob\/master\/vocabularies\/Org\.OData\./g" ^
+            -e "s/<a id=\"user-content-[[:alpha:]-]\+\" class=\"anchor\" href=\"#[[:alpha:]-]\+\" aria-hidden=\"true\"><\/a>//g" ^
+            -e "s/\"Common\.xml\"/\"https:\/\/wiki.scn.sap.com\/wiki\/download\/attachments\/448470974\/Common.xml?api=v2\"/g" ^
+            -e "s/\"Communication\.xml\"/\"https:\/\/wiki.scn.sap.com\/wiki\/download\/attachments\/448470971\/Communication.xml?api=v2\"/g" ^
+            -e "s/\"UI\.xml\"/\"https:\/\/wiki.scn.sap.com\/wiki\/download\/attachments\/448470968\/UI.xml?api=v2\"/g" ^
+            -e "s/com\.sap\.vocabularies\.\([^.]\+\)\.v1\.md#/https:\/\/wiki.scn.sap.com\/wiki\/display\/EmTech\/OData+4.0+Vocabularies+-+SAP+\1#/g" ^
+            -e "s/\"Common\.md#/\"https:\/\/wiki.scn.sap.com\/wiki\/display\/EmTech\/OData+4.0+Vocabularies+-+SAP+Common#/g" ^
+            -e "s/\"Communication\.md#/\"https:\/\/wiki.scn.sap.com\/wiki\/display\/EmTech\/OData+4.0+Vocabularies+-+SAP+Communication#/g" ^
+            %~n1.html > %~n1.scn 
+    del %~n1.html
+  )
+  echo:
 
 exit /b
