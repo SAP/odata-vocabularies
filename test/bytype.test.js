75ed00d67f296ab668d06ef090f2607b1d453977
D024504 <heiko.theissen@sap.com>
2023-09-06 08:34:20 +0200

    Consider functions, add test case

---
+++ test/bytype.test.js
@@ -0,0 +1,21 @@
+const assert = require("assert");
+const bytype = require("../lib/bytype");
+
+describe("Find by type", function () {
+  it("Find by type", function () {
+    assert.deepStrictEqual(
+      bytype(
+        __dirname + "/",
+        "com.sap.vocabularies.Common.v1",
+        "QualifiedName"
+      ),
+      [
+        "com.sap.vocabularies.test.v1.SimpleTerm",
+        "com.sap.vocabularies.test.v1.ComplexTerm/Name",
+        "com.sap.vocabularies.test.v1.Function/SingleParam",
+        "com.sap.vocabularies.test.v1.Function/DoubleParam",
+        "com.sap.vocabularies.test.v1.Function/$ReturnType/Name",
+      ]
+    );
+  });
+});
