838728cb640ae9026bb25d46a1cdcb8f4d7736d8
D024504 <heiko.theissen@sap.com>
2023-09-06 11:55:17 +0200

    Analyse terms and complex types

--- test/bytype.test.js
+++ test/bytype.test.js
@@ -12,9 +12,11 @@ describe("Find by type", function () {
       [
         "com.sap.vocabularies.test.v1.SimpleTerm",
         "com.sap.vocabularies.test.v1.ComplexTerm/Name",
+        "com.sap.vocabularies.test.v1.ComplexType/Name",
+        "com.sap.vocabularies.test.v1.DerivedType/Name",
         "com.sap.vocabularies.test.v1.Function/SingleParam",
         "com.sap.vocabularies.test.v1.Function/DoubleParam",
-        "com.sap.vocabularies.test.v1.Function/$ReturnType/Name",
+        "com.sap.vocabularies.test.v1.Function/$ReturnType/Name"
       ]
     );
   });
