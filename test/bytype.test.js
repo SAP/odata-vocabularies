a3ffde1393384fa9e72f8641a0baf2ae8f60b0af
D024504 <heiko.theissen@sap.com>
2023-09-07 08:53:19 +0200

    Multiple types

--- test/bytype.test.js
+++ test/bytype.test.js
@@ -4,19 +4,20 @@ const bytype = require("../lib/bytype");
 describe("Find by type", function () {
   it("Find by type", function () {
     assert.deepStrictEqual(
-      bytype(
-        __dirname + "/",
-        "com.sap.vocabularies.Common.v1",
-        "QualifiedName"
-      ),
+      bytype(__dirname + "/", [
+        {
+          nsType: "com.sap.vocabularies.Common.v1",
+          localType: "QualifiedName",
+        },
+      ]).sort(),
       [
-        "com.sap.vocabularies.test.v1.SimpleTerm",
         "com.sap.vocabularies.test.v1.ComplexTerm/Name",
         "com.sap.vocabularies.test.v1.ComplexType/Name",
         "com.sap.vocabularies.test.v1.DerivedType/Name",
-        "com.sap.vocabularies.test.v1.Function/SingleParam",
+        "com.sap.vocabularies.test.v1.Function/$ReturnType/Name",
         "com.sap.vocabularies.test.v1.Function/DoubleParam",
-        "com.sap.vocabularies.test.v1.Function/$ReturnType/Name"
+        "com.sap.vocabularies.test.v1.Function/SingleParam",
+        "com.sap.vocabularies.test.v1.SimpleTerm",
       ]
     );
   });
